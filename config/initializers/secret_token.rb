73c5a27d26bbd886ddf6fa8de308614127a952d1
masaki Masuda <maskin@metamix.com>
2013-08-17 20:59:47 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkHeroku::Application.config.secret_token = 'b52818d6b607fcb39e79e4c073fb131b5d29d0ae734335276a3a85aab7af33f94b09caeb7d8f3e115dc65a2093c1e61ee9f6454718e03e5841d1368ad9b718a5'
